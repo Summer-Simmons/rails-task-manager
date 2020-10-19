6e58df108cc2b71ec82a265d85c56a6bb4e659d6
Summer S <summer.s2020@icloud.com>
2020-10-20 01:20:10 +0900

    index page done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,24 @@
 class TasksController < ApplicationController
-  def all
-    @tasks
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
   end
 end
